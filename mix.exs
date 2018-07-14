4e45359721eae6af5421233c2d9a2cb1610aa2a6
Yury Gargay <yury.gargay@gmail.com>
2018-07-14 22:57:09 +0200

    Update travis.yml for Erlang and Elixir versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExTermStorage.Mixfile do
     [
       app: :ex_term_storage,
       version: "0.1.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
